d0c14b0d010ef491eb3d053e87537dc10fe96575
ByP0 <andryhan09@yandex.com>
2025-04-18 11:27:35 +0200

    add local postgres

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
 from app.api.forts_routes import router as forts_router
 from app.api.users_routers import router as users_router
 from app.api.tours_routes import router as tours_router
+from app.database.postgres import create_tables
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    yield
 
-origins = [
-    "http://localhost",
-    "http://localhost:5173"
-]
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
