8392e83fa2e9c8318a9e869b18540e2c9de76550
ByP0 <andryhan09@yandex.com>
2025-04-22 16:03:34 +0200

    fix errors with db

--- backend/app/database/postgres.py
+++ backend/app/database/postgres.py
@@ -19,6 +19,14 @@ async def create_tables():
         await conn.run_sync(Base.metadata.create_all)
     await engine.dispose()
 
+async_session_for_upload = async_sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+        autoflush=False,
+        autocommit=False,
+    )
+
 async def get_session():
     async_session = async_sessionmaker(
         bind=engine,
@@ -31,38 +39,39 @@ async def get_session():
         yield session
         await session.close()
 
-async def upload_forts(session: AsyncSession, image_folder_path: str):
-    try:
-        for i in range(len(forts_id_list)):
-            data_for_db = Forts(
-                fort_id=forts_id_list[i],
-                fort_name=forts_name_list[i],
-                description=forts_descriptions[i]
-            )
-            session.add(data_for_db)
-            await session.commit()
-            await session.refresh(data_for_db)
-
-            image_filename = f"{forts_id_list[i]}.png"
-            image_path = os.path.join(image_folder_path, image_filename)
-
-            if os.path.isfile(image_path):
-                with open(image_path, 'rb') as image_file:
-                    image_data = image_file.read()
-
-                image_for_db = Image(
-                    filename=image_filename,
-                    content_type='image/png',
-                    image_data=image_data,
-                    fort_id=data_for_db.fort_id
+async def upload_forts(image_folder_path: str):
+    async with async_session_for_upload() as session:
+        try:
+            for i in range(len(forts_id_list)):
+                data_for_db = Forts(
+                    fort_id=forts_id_list[i],
+                    fort_name=forts_name_list[i],
+                    description=forts_descriptions[i]
                 )
+                session.add(data_for_db)
+                await session.commit()
+                await session.refresh(data_for_db)
 
-                session.add(image_for_db)
-            else:
-                print(f"Warning: Image file {image_filename} does not exist, skipping.")
+                image_filename = f"{forts_id_list[i]}.png"
+                image_path = os.path.join(image_folder_path, image_filename)
+
+                if os.path.isfile(image_path):
+                    with open(image_path, 'rb') as image_file:
+                        image_data = image_file.read()
+
+                    image_for_db = Image(
+                        filename=image_filename,
+                        content_type='image/png',
+                        image_data=image_data,
+                        fort_id=data_for_db.fort_id
+                    )
+
+                    session.add(image_for_db)
+                else:
+                    print(f"Warning: Image file {image_filename} does not exist, skipping.")
+                
+            await session.commit()
             
-        await session.commit()
-        
-    except Exception as e:
-        await session.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
+        except Exception as e:
+            await session.rollback()
+            raise HTTPException(status_code=500, detail=str(e))
--- backend/app/main.py
+++ backend/app/main.py
@@ -5,14 +5,13 @@ from contextlib import asynccontextmanager
 from app.api.forts_routes import router as forts_router
 from app.api.users_routers import router as users_router
 from app.api.tours_routes import router as tours_router
-from app.database.postgres import upload_forts, get_session, create_tables
+from app.database.postgres import upload_forts, create_tables
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await create_tables()
-    async with get_session() as session:
-        await upload_forts(session, "app/database/data_forts")
-        yield
+    await upload_forts("app/database/data_forts")
+    yield
 
 
 app = FastAPI(lifespan=lifespan)
