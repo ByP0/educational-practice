4bcddfcc7b98bb2457bc722821e4d57df5bfd6fb
ByP0 <andryhan09@yandex.com>
2025-04-22 15:49:09 +0200

    /

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,10 +5,11 @@ from contextlib import asynccontextmanager
 from app.api.forts_routes import router as forts_router
 from app.api.users_routers import router as users_router
 from app.api.tours_routes import router as tours_router
-from app.database.postgres import upload_forts, get_session
+from app.database.postgres import upload_forts, get_session, create_tables
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await create_tables()
     async with get_session() as session:
         await upload_forts(session, "app/database/data_forts")
         yield
