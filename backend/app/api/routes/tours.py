bb47614e7e987c4107fb1dfdd4793d4c3aa59bf6
ByP0 <andryhan09@yandex.com>
2025-04-28 23:51:24 +0200

    update tours routes

--- backend/app/api/routes/tours.py
+++ backend/app/api/routes/tours.py
@@ -1,48 +1,43 @@
-from fastapi import APIRouter, Depends, HTTPException, Query, Header, Path
+from fastapi import APIRouter, Depends, Query, Request
 from typing import Annotated, Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.postgres import get_session
-from app.cruds.tours_cruds import get_tours_by_fort_id, delete_tour_db, add_tour_db, patch_tour_tb, register_user_to_tour, get_user_tours, get_user_register_tours
+from app.cruds.tours_cruds import get_tours_by_fort_id, delete_tour_db, add_tour_db, patch_tour_tb, get_user_tours
 from app.schemas.tours_schemas import ToursData, TourAdd, TourPatch
 from app.schemas.response_schemas import Response200
-from app.cruds.users_cruds import check_session
+from app.core.security import get_token_data, dependencies
 
 
-router = APIRouter(tags=["Tours"], prefix="/tours")
+router = APIRouter(tags=["Tours"], prefix="/tours", dependencies=dependencies)
 
 
 @router.post("/add", response_model=Response200)
 async def add_tour(
+    request: Request,
     data: TourAdd,
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
     session: AsyncSession = Depends(get_session),
 ):
-    user = await check_session(user_session=user_session, session=session)
-    try:
-        await add_tour_db(data=data, session=session, user_id=user)
-        return Response200
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Server Error {str(e)}")
+    token_data = get_token_data(request=request)
+    await add_tour_db(data=data, session=session, user_id=token_data.get("sub"))
+    return Response200
     
     
 @router.get("/my", response_model=list[ToursData])
 async def get_my_tours(
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
+    request: Request,
     session: AsyncSession = Depends(get_session),
 ):
-    user_id = await check_session(user_session=user_session, session=session)
-    tours = await get_user_tours(user_id=user_id, session=session)
+    token_data = get_token_data(request=request)
+    tours = await get_user_tours(user_id=token_data.get("sub"), session=session)
     return tours
 
 
 @router.get("/", response_model=list[ToursData])
 async def get_tours_fort(
     fort_id: Annotated[Optional[int], Query(title="Fort ID", example=1)],
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
     session: AsyncSession = Depends(get_session),
 ):
-    await check_session(user_session=user_session, session=session)
     tours = await get_tours_by_fort_id(fort_id=fort_id, session=session)
     return tours
 
@@ -50,10 +45,8 @@ async def get_tours_fort(
 @router.delete("/delete", response_model=Response200)
 async def delete_tour(
     tour_id: Annotated[Optional[int], Query(title="Fort ID", example=1)],
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
     session: AsyncSession = Depends(get_session),
 ):
-    await check_session(user_session=user_session, session=session)
     await delete_tour_db(tour_id=tour_id, session=session)
     return Response200(detail="Succesful deleted")
     
@@ -62,29 +55,7 @@ async def delete_tour(
 async def patch_tour(
     new_data: TourPatch,
     tour_id: Annotated[Optional[int], Query(title="Fort ID", example=1)],
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
     session: AsyncSession = Depends(get_session),
 ):
-    await check_session(user_session=user_session, session=session)
     await patch_tour_tb(tour_id=tour_id, data=new_data, session=session)
     return Response200
-
-
-@router.post("/register/{tour_id}", response_model=Response200)
-async def register_to_tour(
-    tour_id: Annotated[int, Path(title="Tour ID", example=1)],
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
-    session: AsyncSession = Depends(get_session),
-):
-    user_id = await check_session(user_session=user_session, session=session)
-    await register_user_to_tour(user_id=user_id, tour_id=tour_id, session=session)
-    return Response200
-
-@router.get("/register/my" ,response_model=list[ToursData])
-async def get_my_registers_tours(
-    user_session: Annotated[str, Header(title="User session", example="123e4567-e89b-12d3-a456-426614174000")],
-    session: AsyncSession = Depends(get_session),
-):
-    user_id = await check_session(user_session=user_session, session=session)
-    tours = await get_user_register_tours(user_id=user_id, session=session)
-    return tours
--- backend/app/cruds/tours_cruds.py
+++ backend/app/cruds/tours_cruds.py
@@ -9,149 +9,179 @@ from app.schemas.tours_schemas import ToursData, TourAdd, TourPatch
 
 
 async def get_tours_by_fort_id(fort_id: int, session: AsyncSession) -> list[ToursData]:
-    current_time = datetime.now()
+    try:
+        current_time = datetime.now()
 
-    stmt = (
-        select(
-            Tours,
-            Forts.fort_name,
-            Image.image_id,
-            Image.filename,
-            Image.content_type,
-            Image.image_data
+        stmt = (
+            select(
+                Tours,
+                Forts.fort_name,
+                Image.image_id,
+                Image.filename,
+                Image.content_type,
+                Image.image_data
+            )
+            .join(Forts, Tours.fort_id == Forts.fort_id)
+            .outerjoin(Image, Tours.fort_id == Image.fort_id)
+            .where(Tours.tour_date >= current_time)
+            .order_by(Tours.tour_id)
         )
-        .join(Forts, Tours.fort_id == Forts.fort_id)
-        .outerjoin(Image, Tours.fort_id == Image.fort_id)
-        .where(Tours.tour_date >= current_time)
-        .order_by(Tours.tour_id)
-    )
 
-    result: Result = await session.execute(stmt)
-    tours = result.all()
+        result: Result = await session.execute(stmt)
+        tours = result.all()
 
-    if not tours:
-        raise HTTPException(status_code=404, detail="Tours not found")
+        if not tours:
+            raise HTTPException(status_code=404, detail="Tours not found")
 
-    tours_data = []
-    for tour, fort_name, image_id, filename, content_type, image_data in tours:
-        encoded_image_data = None
-        if image_data:
-            encoded_image_data = base64.b64encode(image_data).decode('utf-8')
+        tours_data = []
+        for tour, fort_name, image_id, filename, content_type, image_data in tours:
+            encoded_image_data = None
+            if image_data:
+                encoded_image_data = base64.b64encode(image_data).decode('utf-8')
 
-        tours_data.append(ToursData(
-            tour_id=tour.tour_id,
-            gathering_place=tour.gathering_place,
-            tour_date=tour.tour_date,
-            number_of_seats=tour.number_of_seats,
-            cost=tour.cost,
-            fort_id=tour.fort_id,
-            fort_name=fort_name if fort_name else "",
-            image={
-                "image_id": image_id,
-                "filename": filename,
-                "content_type": content_type,
-                "image_data": encoded_image_data
-            } if image_data else None
-        ))
+            tours_data.append(ToursData(
+                tour_id=tour.tour_id,
+                gathering_place=tour.gathering_place,
+                tour_date=tour.tour_date,
+                number_of_seats=tour.number_of_seats,
+                cost=tour.cost,
+                fort_id=tour.fort_id,
+                fort_name=fort_name if fort_name else "",
+                image={
+                    "image_id": image_id,
+                    "filename": filename,
+                    "content_type": content_type,
+                    "image_data": encoded_image_data
+                } if image_data else None
+            ))
 
-    unique_tours = {tour.tour_id: tour for tour in tours_data}
+        unique_tours = {tour.tour_id: tour for tour in tours_data}
 
-    return list(unique_tours.values())
+        return list(unique_tours.values())
+    
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 async def get_user_tours(user_id: int, session: AsyncSession) -> list[ToursData]:
-    current_time = datetime.now()
+    try:
+        current_time = datetime.now()
 
-    stmt = (
-        select(
-            Tours,
-            Forts.fort_name,
-            Image.image_id,
-            Image.filename,
-            Image.content_type,
-            Image.image_data
+        stmt = (
+            select(
+                Tours,
+                Forts.fort_name,
+                Image.image_id,
+                Image.filename,
+                Image.content_type,
+                Image.image_data
+            )
+            .join(Forts, Tours.fort_id == Forts.fort_id)
+            .outerjoin(Image, Tours.fort_id == Image.fort_id)
+            .where(Tours.user_id == user_id)
+            .where(Tours.tour_date >= current_time)
+            .order_by(Tours.tour_id)
         )
-        .join(Forts, Tours.fort_id == Forts.fort_id)
-        .outerjoin(Image, Tours.fort_id == Image.fort_id)
-        .where(Tours.user_id == user_id)
-        .where(Tours.tour_date >= current_time)
-        .order_by(Tours.tour_id)
-    )
 
-    result: Result = await session.execute(stmt)
-    tours = result.all()
+        result: Result = await session.execute(stmt)
+        tours = result.all()
 
-    if not tours:
-        raise HTTPException(status_code=404, detail="Tours not found")
+        if not tours:
+            raise HTTPException(status_code=404, detail="Tours not found")
 
-    tours_data = []
-    for tour, fort_name, image_id, filename, content_type, image_data in tours:
-        encoded_image_data = None
-        if image_data:
-            encoded_image_data = base64.b64encode(image_data).decode('utf-8')
+        tours_data = []
+        for tour, fort_name, image_id, filename, content_type, image_data in tours:
+            encoded_image_data = None
+            if image_data:
+                encoded_image_data = base64.b64encode(image_data).decode('utf-8')
 
-        tours_data.append(ToursData(
-            tour_id=tour.tour_id,
-            gathering_place=tour.gathering_place,
-            tour_date=tour.tour_date,
-            number_of_seats=tour.number_of_seats,
-            cost=tour.cost,
-            fort_id=tour.fort_id,
-            fort_name=fort_name if fort_name else "",
-            image={
-                "image_id": image_id,
-                "filename": filename,
-                "content_type": content_type,
-                "image_data": encoded_image_data
-            } if image_data else None
-        ))
+            tours_data.append(ToursData(
+                tour_id=tour.tour_id,
+                gathering_place=tour.gathering_place,
+                tour_date=tour.tour_date,
+                number_of_seats=tour.number_of_seats,
+                cost=tour.cost,
+                fort_id=tour.fort_id,
+                fort_name=fort_name if fort_name else "",
+                image={
+                    "image_id": image_id,
+                    "filename": filename,
+                    "content_type": content_type,
+                    "image_data": encoded_image_data
+                } if image_data else None
+            ))
 
-    unique_tours = {tour.tour_id: tour for tour in tours_data}
+        unique_tours = {tour.tour_id: tour for tour in tours_data}
 
-    return list(unique_tours.values())
+        return list(unique_tours.values())
+    
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 async def delete_tour_db(tour_id: int, session: AsyncSession):
-    stmt = select(Tours).where(Tours.tour_id == tour_id)
-    result: Result = await session.execute(stmt)
-    tour = result.scalar_one_or_none()
+    try:
+        stmt = select(Tours).where(Tours.tour_id == tour_id)
+        result: Result = await session.execute(stmt)
+        tour = result.scalar_one_or_none()
 
-    if tour is None:
-        raise HTTPException(status_code=404, detail="Tour not found")
+        if tour is None:
+            raise HTTPException(status_code=404, detail="Tour not found")
 
-    await session.delete(tour)
-    await session.commit()
+        await session.delete(tour)
+        await session.commit()
+
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 async def add_tour_db(data: TourAdd, session: AsyncSession, user_id: int):
-    data_for_db = Tours(
-        gathering_place=data.gathering_place,
-        tour_date=data.tour_date,
-        number_of_seats=data.number_of_seats,
-        cost=data.cost,
-        fort_id=data.fort_id,
-        user_id=user_id,
-    )
-    session.add(data_for_db)
-    await session.commit()
-    await session.refresh(data_for_db)
+    try:
+        data_for_db = Tours(
+            gathering_place=data.gathering_place,
+            tour_date=data.tour_date,
+            number_of_seats=data.number_of_seats,
+            cost=data.cost,
+            fort_id=data.fort_id,
+            user_id=user_id,
+        )
+        session.add(data_for_db)
+        await session.commit()
+        await session.refresh(data_for_db)
+    
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
 async def patch_tour_tb(tour_id: int, data: TourPatch, session: AsyncSession):
-    stmt = select(Tours).where(Tours.tour_id == tour_id)
-    
-    result: Result = await session.execute(stmt)
-    tour = result.scalar_one_or_none()
-    
-    if tour is None:
-        raise HTTPException(status_code=404, detail=f"Tour with id {tour_id} not found.")
+    try:
+        stmt = select(Tours).where(Tours.tour_id == tour_id)
+        
+        result: Result = await session.execute(stmt)
+        tour = result.scalar_one_or_none()
+        
+        if tour is None:
+            raise HTTPException(status_code=404, detail=f"Tour with id {tour_id} not found.")
 
-    if data.gathering_place is not None:
-        tour.gathering_place = data.gathering_place
-    if data.tour_date is not None:
-        tour.tour_date = data.tour_date
-    if data.number_of_seats is not None:
-        tour.number_of_seats = data.number_of_seats
-    if data.cost is not None:
-        tour.cost = data.cost
-    
-    await session.commit()
+        if data.gathering_place is not None:
+            tour.gathering_place = data.gathering_place
+        if data.tour_date is not None:
+            tour.tour_date = data.tour_date
+        if data.number_of_seats is not None:
+            tour.number_of_seats = data.number_of_seats
+        if data.cost is not None:
+            tour.cost = data.cost
+        
+        await session.commit()
+
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 async def register_user_to_tour(user_id: int, tour_id: int, session: AsyncSession):
     try:
@@ -187,49 +217,55 @@ async def register_user_to_tour(user_id: int, tour_id: int, session: AsyncSessio
         raise HTTPException(status_code=500, detail=str(e))
     
 async def get_user_register_tours(user_id: int, session: AsyncSession) -> list[ToursData]:
-    current_time = datetime.now()
+    try:
+        current_time = datetime.now()
 
-    stmt = (
-        select(
-            Tours,
-            Forts.fort_name,
-            Image.image_id,
-            Image.filename,
-            Image.content_type,
-            Image.image_data
+        stmt = (
+            select(
+                Tours,
+                Forts.fort_name,
+                Image.image_id,
+                Image.filename,
+                Image.content_type,
+                Image.image_data
+            )
+            .join(UserTours, UserTours.tour_id == Tours.tour_id)
+            .join(Forts, Tours.fort_id == Forts.fort_id)
+            .outerjoin(Image, Image.fort_id == Tours.fort_id)
+            .where(UserTours.user_id == user_id)
+            .where(Tours.tour_date >= current_time)
+            .order_by(Tours.tour_id)
         )
-        .join(UserTours, UserTours.tour_id == Tours.tour_id)
-        .join(Forts, Tours.fort_id == Forts.fort_id)
-        .outerjoin(Image, Image.fort_id == Tours.fort_id)
-        .where(UserTours.user_id == user_id)
-        .where(Tours.tour_date >= current_time)
-        .order_by(Tours.tour_id)
-    )
 
-    result: Result = await session.execute(stmt)
-    tour_rows = result.all()
+        result: Result = await session.execute(stmt)
+        tour_rows = result.all()
 
-    if not tour_rows:
-        raise HTTPException(status_code=404, detail="Tours not found")
+        if not tour_rows:
+            raise HTTPException(status_code=404, detail="Tours not found")
 
-    unique_tours = {}
+        unique_tours = {}
 
-    for tour, fort_name, image_id, filename, content_type, image_data in tour_rows:
-        encoded_image_data = base64.b64encode(image_data).decode('utf-8') if image_data else None
-        unique_tours.setdefault(tour.tour_id, ToursData(
-            tour_id=tour.tour_id,
-            gathering_place=tour.gathering_place,
-            tour_date=tour.tour_date,
-            number_of_seats=tour.number_of_seats,
-            cost=tour.cost,
-            fort_id=tour.fort_id,
-            fort_name=fort_name or "",
-            image={
-                "image_id": image_id,
-                "filename": filename,
-                "content_type": content_type,
-                "image_data": encoded_image_data
-            } if image_data else None
-        ))
+        for tour, fort_name, image_id, filename, content_type, image_data in tour_rows:
+            encoded_image_data = base64.b64encode(image_data).decode('utf-8') if image_data else None
+            unique_tours.setdefault(tour.tour_id, ToursData(
+                tour_id=tour.tour_id,
+                gathering_place=tour.gathering_place,
+                tour_date=tour.tour_date,
+                number_of_seats=tour.number_of_seats,
+                cost=tour.cost,
+                fort_id=tour.fort_id,
+                fort_name=fort_name or "",
+                image={
+                    "image_id": image_id,
+                    "filename": filename,
+                    "content_type": content_type,
+                    "image_data": encoded_image_data
+                } if image_data else None
+            ))
 
-    return list(unique_tours.values())
+        return list(unique_tours.values())
+    
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
